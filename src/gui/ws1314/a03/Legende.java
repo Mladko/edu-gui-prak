ab7988efd9684e97b8d9c190308b05ccc062c197
agribu <agribu@online.de>
2013-10-30 16:56:35 +0100

    first draft

---
+++ src/gui/ws1314/a03/Legende.java
@@ -0,0 +1,168 @@
+package gui.ws1314.a03;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import javax.swing.JPanel;
+
+/**
+ * Erzeugt die Legende zu dem in ReifeDiagramm.java generierten Balkendiagramm.
+ *
+ * @author Benjamin Schuermann <agribu>
+ * @since 2013/10/21
+ * @version 1.0
+ */
+public class Legende extends JPanel {
+    
+    /**
+     * Setzt den Status fuer das Zeichnen der Legende.
+     */
+    public boolean zeichneLegende;
+    
+    // Grafikobjekt
+    private Graphics2D g;
+    // Schriftgroesse
+    private int schriftgroesse;
+    // Position und Groessen des Diagramms
+    private double x, y, b, h;
+    // Groessen des Fensters
+    private double fensterBreite, fensterHoehe;
+    // Sammlung der Farben aller Stadien
+    private final Object[] COLORS = {Color.gray,
+        new GradientPaint(0, 0, Color.gray, 0, 0, Color.green),
+        Color.green, Color.yellow};
+
+    /**
+     * Erzeugt ein neues Legendeobjekt.
+     */
+    public Legende() {
+        // Setze weisse Hintergrundfarbe
+        this.setBackground(Color.white);
+        
+        // Legende soll gezeichnet werden
+        this.zeichneLegende = true;
+    }
+
+    /**
+     * Stellt eine neue Legende grafisch dar.
+     */
+    public void erstelleLegende() {
+        // Groesse eines Legendekastens, bzw. Zwischenraums
+        double size = this.h / 9;
+        // Beschriftungen zur Stadien der Trinkreife
+        String[] stadien = {"Zu früh", "Steigert sich noch", 
+            "Optimaler Trinkzeitpunkt", "Überlagert"};
+
+        // this.g.draw(new Rectangle.Double(this.x, this.y, this.b, this.h));
+        
+        // Setze Titel
+        this.g.drawString(
+                "Legende",
+                (int) (this.x),
+                (int) (this.y + schriftgroesse));
+        
+        for(int i = 0; i < 4; i++) {
+            // Berechne Position
+            this.y += size * 2;
+            
+            // Setze Fuellfarbe
+            if (this.COLORS[i] instanceof GradientPaint) {
+                this.COLORS[i] = new GradientPaint(
+                        (int) this.x, (int) this.y, Color.gray,
+                        (int) (this.x + size), (int) this.y, Color.green);
+            }
+            this.g.setPaint((Paint) this.COLORS[i]);
+            this.g.fill(
+                    new Rectangle.Double(this.x, this.y, size, size));
+
+            // Zuruecksetzen der Farbe
+            this.g.setPaint(Color.black);
+            
+            // Zeichne Aussenrahmen
+            this.g.draw(new Rectangle.Double(this.x, this.y, size, size));
+            
+            // Setze Beschriftung
+            this.g.drawString(
+                    stadien[i],
+                    (int) (this.x + (size * 2)),
+                    (int) (this.y + size));
+        }
+    }
+
+    /**
+     * Setzt allgemeine Eigenschaften.
+     *
+     * @param d Dimension des Fensters
+     * @param g Grafikobjekt
+     */
+    public void setzeEigenschaften(Dimension d, Graphics2D g) {
+        this.fensterBreite = (int) d.width;
+        this.fensterHoehe = (int) d.height;
+        
+        // Setze Grafikobjekt
+        this.g = g;
+
+        // Setzt die aktuelle Schritgroesse
+        FontMetrics metrics = g.getFontMetrics();
+        this.schriftgroesse = metrics.getHeight();
+        
+        // Aktualisierung der Groessen der Legende
+        this.setzeLegende();
+    }
+    
+    /**
+     * Setzt das Textformat des Inhalts
+     * 
+     * @param g Grafikobjekt
+     */
+    public void setzeTextFormat(Graphics2D g) {
+        // Setze Schriftgroesse und Schriftart
+        int fontSize = (int) Math.round(1.5 * this.getWidth() / 72.0);
+        Font font = new Font("Arial", Font.PLAIN, fontSize);
+        g.setFont(font);
+    }
+    
+    /**
+     * Stellt den Inhalt grafisch dar.
+     *
+     * @param g Grafikobjekt.
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        // Aktiviere Antialiasing (Schriftenglaettung)
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Uebergeben der Fenstergroesse und des Grafikobjekts
+        this.setzeEigenschaften(this.getSize(), g2);
+        
+        // Textformat
+        this.setzeTextFormat(g2);
+
+        super.paintComponent(g2);
+
+        // Zeichne Legende
+        if (zeichneLegende)
+            this.erstelleLegende();
+    }
+    
+    /**
+     * Setzt die Groesse und Position der Legende.
+     */
+    private void setzeLegende() {
+        this.x = (this.fensterBreite * 10) / 100;   // x-Position
+        this.y = (this.fensterHoehe * 10) / 100;    // y-Position
+        this.b = (this.fensterBreite * 80) / 100;   // Breite
+        this.h = (this.fensterHoehe * 80) / 100;    // Hoehe      
+    }
+    
+}
---
+++ src/gui/ws1314/a03/ReifeDiagramm.java
@@ -0,0 +1,274 @@
+package gui.ws1314.a03;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.util.Calendar;
+import javax.swing.JPanel;
+
+/**
+ * Erzeugt ein Balkendiagramm zur Darstellung der Trinkreife eines Weines.
+ *
+ * @author Benjamin Schuermann <agribu>
+ * @since 2013/10/21
+ * @version 1.0
+ */
+public class ReifeDiagramm extends JPanel {
+    /**
+     * Stadien der Trinkreife.
+     */ 
+    public Stadium[] stadien;
+    
+    // Grafikobjekt
+    private Graphics2D g;
+    // Schriftgroesse
+    private int schriftgroesse;
+    // Position und Groessen des Diagramms
+    private double x, y, b, h;
+    // Groessen des Fensters
+    private double fensterBreite, fensterHoehe;
+    // Startjahr des aktuellen Stadiums
+    private int stadiumBeginn;
+    // Aktuelle Jahr
+    private int aktJahr;
+    // Tastatureingaben zu den Eigenschaften eines Weines
+    private double lagerdauer, jahrgang;
+    // Farbsetzung
+    public Object[] farben = {Color.gray,
+            new GradientPaint(0, 0, Color.gray, 0, 0, Color.green),
+            Color.green, Color.yellow};
+
+    /**
+     * Erzeugt ein neues Legendeobjekt.
+     */
+    public ReifeDiagramm() {
+        // Setze weisse Hintergrundfarbe
+        this.setBackground(Color.white);
+    }
+
+    /**
+     * Stellt ein neues Digramm grafisch dar.
+     */
+    public void erstelleDiagramm() {
+        // Ausserer Rahmen des Diagramms
+        this.g.draw(new Rectangle.Double(this.x, this.y, this.b, this.h));
+        // Stadien der Trinkreife
+        zeichneStadien();
+        // Aktuelles Jahr
+        zeichneAktuellesJahr();
+    }
+
+    /**
+     * Setzt allgemeine Eigenschaften.
+     *
+     * @param d Dimension des Fensters
+     * @param g Grafikobjekt
+     */
+    public void setzeEigenschaften(Dimension d, Graphics2D g) {
+        // Setze Fenstergroesse
+        this.fensterBreite = (double) d.width;
+        this.fensterHoehe = (double) d.height;
+
+        // Setze Grafikobjekt
+        this.g = g;
+
+        // Setzt die aktuelle Schritgroesse
+        FontMetrics metrics = g.getFontMetrics();
+        this.schriftgroesse = metrics.getHeight();
+
+        // Setzt das aktuelle Jahr
+        this.aktJahr = Calendar.getInstance().get(Calendar.YEAR);
+
+        // Aktualisierung der Groessen des Diagramms
+        this.setzeDiagramm();
+        
+        // Setzt die Stadien der Trinkreife
+        this.setzeStadien();
+    }
+
+    /**
+     * Setzt den Jahrgang und die Lagerdauer.
+     *
+     * @param jahrgang Jahrgang
+     * @param lagerdauer Lagerdauer
+     */
+    public void setzeEingabe(int jahrgang, int lagerdauer) {
+        this.jahrgang = jahrgang;
+        this.lagerdauer = lagerdauer;
+    }
+
+    /**
+     * Setzt das Textformat des Inhalts
+     * 
+     * @param g Grafikobjekt
+     */
+    public void setzeTextFormat(Graphics2D g) {
+        // Setze Schriftgroesse und Schriftart
+        int fontSize = (int) Math.round(1.5 * this.getWidth() / 72.0);
+        Font font = new Font("Arial", Font.PLAIN, fontSize);
+        g.setFont(font);
+    }
+    
+    /**
+     * Zeichnet das aktuelle Stadium.
+     * 
+     * @param s Stadium
+     * @param position Position
+     */
+    public void zeichneAktStadium(Stadium s, double position) {
+        // Setze Fuellfarbe
+        if (s.farbe instanceof GradientPaint) {
+            s.setzeFarbe(new GradientPaint((int) position, 
+                    (int) this.y, ((GradientPaint) s.farbe).getColor1(), 
+                    (int) (position + s.breite), 
+                    (int) this.y, ((GradientPaint) s.farbe).getColor2()));
+        }
+        this.g.setPaint((Paint) s.farbe);
+        this.g.fill(new Rectangle.Double(
+                position, this.y, s.breite, this.h));
+        // Zuruecksetzen der Farbe
+        this.g.setPaint(Color.black);
+        // Zeichne Rahmen des aktuellen Stadiums und setze die Beschriftung
+        this.g.draw(new Rectangle.Double(
+                position, this.y, s.breite, this.h));
+        if (stadiumBeginn != this.aktJahr) {
+            this.g.drawString(
+                    Integer.toString(stadiumBeginn),
+                    (int) position,
+                    (int) (this.y + this.h + this.schriftgroesse));
+        }
+    }
+    
+    /**
+     * Stellt den Inhalt grafisch dar.
+     *
+     * @param g Grafikobjekt.
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        // Aktiviere Antialiasing (Schriftenglaettung)
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Uebergeben der Fenstergroesse und des Grafikobjekts
+        this.setzeEigenschaften(this.getSize(), g2);
+        
+        // Textformat
+        this.setzeTextFormat(g2);
+
+        super.paintComponent(g2);
+
+        // Zeichne Diagramm
+        this.erstelleDiagramm();
+    }
+
+    /**
+     * Setzt die Groesse und Position der Legende.
+     */
+    private void setzeDiagramm() {
+        this.x = (this.fensterBreite * 10) / 100;   // x-Position
+        this.y = (this.fensterHoehe * 10) / 100;    // y-Position
+        this.b = (this.fensterBreite * 80) / 100;   // Breite
+        this.h = (this.fensterHoehe * 80) / 100;    // Hoehe
+
+        // Reservierter Platz fuer die Beschriftung des Diagramms
+        this.h -= this.schriftgroesse;
+    }
+
+    /**
+     * Berechnet und setzt die Groessen der Stadien.
+     */
+    private void setzeStadien() {
+        // Berechnung der Dauer
+        double dauerUnreif = Math.round(this.lagerdauer / 8);
+        double dauerOptimal = Math.round(this.lagerdauer / 2);
+        double dauerSteigernd = Math.round(this.lagerdauer
+                - (dauerOptimal + dauerUnreif));
+        double dauerUeberlagert = 1;
+        
+        // Teilberechnung der Breite
+        double breite = this.b / (this.lagerdauer + 1);
+        
+        // Stadien der Trinkreife
+        Stadium unreif = 
+                new Stadium("unreif", dauerUnreif, this.farben[0],
+                        breite * dauerUnreif, this.h, this.y, 0);
+        Stadium steigernd = 
+                new Stadium("reifend", dauerSteigernd, this.farben[1],
+                        breite * dauerSteigernd, this.h, this.y, 1);
+        Stadium optimal = 
+                new Stadium("optimal", dauerOptimal, this.farben[2],
+                        breite * dauerOptimal, this.h, this.y, 2);
+        Stadium ueberlagert = 
+                new Stadium("überlagert", dauerUeberlagert, this.farben[3],
+                        breite * dauerUeberlagert, this.h, this.y, 3);
+
+        // Ablegen als globales Array
+        this.stadien = new Stadium[4];
+        this.stadien[0] = unreif;
+        this.stadien[1] = steigernd;
+        this.stadien[2] = optimal;
+        this.stadien[3] = ueberlagert;
+    }
+
+    /**
+     * Grafische Darstellung aller Stadien.
+     */
+    private void zeichneStadien() {
+        
+        // Setzt den Beginn des ersten Stadiums
+        this.stadiumBeginn = (int) this.jahrgang;
+
+        // Position des aktuellen Stadiums
+        double aktPosition = this.x;
+        for (Stadium aktStadium : this.stadien) {
+            // Setze x-Position und Beginn des aktuellen Stadiums
+            aktStadium.setzeX(aktPosition);
+            aktStadium.setzeBeginn(stadiumBeginn);
+            
+            this.zeichneAktStadium(aktStadium, aktPosition);
+
+            // Berechnet Startjahr des naechsten Stadiums
+            stadiumBeginn += aktStadium.dauer;
+            // Berechne neue Startposition
+            aktPosition += aktStadium.breite;
+        }
+    }
+    
+    /**
+     * Grafische Darstellung des aktuellen Jahr.
+     */
+    private void zeichneAktuellesJahr() {
+        // Breite des Rahmens zum aktuellen Jahr
+        double aktJahrBreite = this.b / (this.lagerdauer + 1);
+        // Position des Rahmens zum aktuellen Jahr
+        double aktJahrPosition = (this.aktJahr - this.jahrgang) * aktJahrBreite;
+
+        if (this.aktJahr >= this.jahrgang && this.aktJahr < stadiumBeginn) {
+            // Zeichne Rahmen des aktuellen Jahrs und setze Beschriftung
+            this.g.setPaint(Color.red);
+            this.g.draw(new Rectangle.Double(
+                    this.x + aktJahrPosition,
+                    this.y - 1,
+                    aktJahrBreite,
+                    this.h + 2));
+            this.g.drawString(
+                    Integer.toString(this.aktJahr),
+                    (int) (this.x + aktJahrPosition),
+                    (int) (this.y + this.h + this.schriftgroesse));
+
+            // Zuruecksetzen der Farbe
+            this.g.setPaint(Color.black);
+        }
+    }
+
+}
